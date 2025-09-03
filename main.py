153db8a02cf2e077f98f55a1cbef5bdf8dc0d42b
amirali <145279276+mazaheryamirali@users.noreply.github.com>
2025-09-03 07:37:00 +0330

    two

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 
 def add_note():
-    note = input("یادداشت امروزت رو بنویس: ")
+    note = input("Write your note for today : ")
     today = datetime.now().strftime("%Y-%m-%d")
     with open(f"{today}.txt", "w", encoding="utf-8") as file:
         file.write(note)
-    print("یادداشت ذخیره شد! ✅")
+    print("Note saved.")
 
 if __name__ == "__main__":
     add_note()
